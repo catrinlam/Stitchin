b29cbf031890854c1bfdc4c4628239476a86108e
Catrin <catrin.lam@gmail.com>
2025-02-04 10:38:37 +0000

    Add pattern detail view and template; update URL routing and styles

--- patterns/views.py
+++ patterns/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Pattern
 
@@ -6,3 +6,26 @@ class PatternList(generic.ListView):
     queryset = Pattern.objects.all().order_by('-created_at')
     template_name = "patterns/index.html"
     paginate_by = 6
+    
+def pattern_detail(request, slug):
+    """
+    Display an individual :model:`pattern.Pattern`.
+
+    **Context**
+
+    ``pattern``
+        An instance of :model:`pattern.Pattern`.
+
+    **Template:**
+
+    :template:`pattern/pattern_detail.html`
+    """
+
+    queryset = Pattern.objects.all()
+    pattern = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "patterns/pattern_detail.html",
+        {"pattern": pattern},
+    )
